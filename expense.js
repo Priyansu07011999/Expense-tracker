4f2712fed970f11bd8f78a8b91369cf772d863f1
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-01-23 23:02:58 +0530

    Add files via upload

---
+++ expense.js
@@ -0,0 +1,63 @@
+function submit_form() {
+    const name = document.getElementById('expenseName').value;
+    const amount = document.getElementById('expenseAmount').value;
+    const index = document.getElementById('expenseIndex').value;
+    const category = document.getElementsByClassName('category').value;
+
+    if (name && amount) {
+      const expense = {name, amount, category};
+      let expenses = JSON.parse(localStorage.getItem('expenses'));
+
+      if (index !== "") {expenses[index] = expense;}   
+      else {expenses.push(expense);}
+      
+
+      localStorage.setItem('expenses', JSON.stringify(expenses)); // set the item in local storage
+
+      // Clear all after submit
+      document.getElementById('expenseName').value = '';
+      document.getElementById('expenseAmount').value = '';
+      document.getElementById('expenseIndex').value = '';
+
+      updateList();
+    }
+  }
+
+  function updateList() {
+    const expenseList = document.getElementById('expenseList');
+    expenseList.innerHTML = '';
+
+    const expenses = JSON.parse(localStorage.getItem('expenses')) ;
+    expenses.forEach(function(expense, index){
+      const listItem = document.createElement('li');
+      listItem.className = 'list';
+      listItem.innerHTML = `${expense.name} -> ${expense.amount}
+                           <button type="button" class="btn btn-secondary mr-2" onclick="editExpense(${index})">Edit-Expenses</button>
+                           <button type="button" class="btn btn-danger" onclick="removeExpense(${index})">Delete-Expenses</button>`;
+      expenseList.appendChild(listItem);
+    });
+  }
+
+  // Function to remove an expense
+  function removeExpense(index) {
+    let expenses = JSON.parse(localStorage.getItem('expenses')) ;
+    expenses.splice(index, 1);
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+    updateList();
+  }
+
+  // Function to edit an expense
+  function editExpense(index) {
+    let expenses = JSON.parse(localStorage.getItem('expenses')) ;
+    const editedExpense = expenses[index];
+
+    document.getElementById('expenseName').value = editedExpense.name;
+    document.getElementById('expenseAmount').value = editedExpense.amount;
+    document.getElementById('expenseIndex').value = index;
+
+    // Update the expense list
+    updateList();
+  }
+
+  // Initial update
+  updateList();
